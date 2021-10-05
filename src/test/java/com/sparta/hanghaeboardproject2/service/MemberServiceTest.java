9f253b7d409eebd5b99843787a078f5cf84f6254
zzangoobrother <com1368@naver.com>
2021-10-05 21:24:20 +0900

    MemeberServiceTest 구현

---
+++ src/test/java/com/sparta/hanghaeboardproject2/service/MemberServiceTest.java
@@ -0,0 +1,14 @@
+package com.sparta.hanghaeboardproject2.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith((MockitoExtension.class))
+class MemberServiceTest {
+
+    
+}
