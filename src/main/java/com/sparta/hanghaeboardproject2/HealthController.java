9369cc79a45f75a0ef2abf05205f198c773031f6
zzangoobrother <com1368@naver.com>
2021-11-13 00:19:34 +0900

    [feat] 무중단 test

--- src/main/java/com/sparta/hanghaeboardproject2/HealthController.java
+++ src/main/java/com/sparta/hanghaeboardproject2/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-  @GetMapping
+  @GetMapping("/test")
   public String healthCheck() {
     return "health ok";
   }
