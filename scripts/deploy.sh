26cd920e1c08aa18b59867ea3b040d0151ca8cb5
zzangoobrother <com1368@naver.com>
2021-11-13 00:02:47 +0900

    [feat] 이메일 인증 만료 시간 스케줄 기능 추가

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-sudo docker ps -a -q --filter "name=hanghae-board-project2" | grep -q . && docker stop hanghae-board-project2 && docker rm hanghae-board-project2 | true
-
-sudo docker rmi backtony/hanghae-board-project2:1.0
-
-sudo docker pull backtony/hanghae-board-project2:1.0
-
-docker run -d -p 8080:8080 --name hanghae-board-project2 backtony/hanghae-board-project2:1.0
-
-docker rmi -f $(docker images -f "dangling=true" -q) || true
